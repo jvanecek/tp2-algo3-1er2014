5dddb8c25559490d09cd5af3e8f92440cff2b92b
Ezequiel R. Aguerre <ezeaguerre@gmail.com>
2014-04-22 11:59:26 -0300

    Cache + bottomup para el 1.

--- src/Ejercicio1/model/Robanumeros.java
+++ src/Ejercicio1/model/Robanumeros.java
@@ -7,36 +7,42 @@ public class Robanumeros {
 	private int[] cartas; /**< Cartas del juego. */
 	private int puntos; /**< Puntaje del jugador 1 del juego finalizado. */
 	private int[][] cache;
-	private boolean[][] validCache;
 
 	private int min( int a, int b ) { return a < b ? a : b; }
+	private int min( int a, int b, int c ) { return min( a, min( b, c ) ); }
 	private int sum( int i, int j ) { int res = 0; for(int k = i; k <= j; k++) res += cartas[k]; return res; }
 
 	public Robanumeros( int[] cartas ){
 		this.cartas = cartas;
 		cache = new int[cartas.length][cartas.length];
-		validCache = new boolean[cartas.length][cartas.length];
 	}
 
-	private int f(int i, int j) {
-		if( i < j ){
-			if ( validCache[i][j] ) return cache[i][j];
-			int min_ij = Integer.MAX_VALUE;
-			for( int k = i; k <= j; k++ ){
-				min_ij = min(min_ij, min(f(i, k - 1), f(k + 1, j)));
+	private void construir() {
+		int n = cartas.length;
+		for ( int i = 0; i < n; i++ )
+			cache[i][i] = cartas[i];
+
+		for ( int columna = 1; columna < n; columna++ ) {
+			for ( int diagonal = 0; diagonal < n - columna; diagonal++ ) {
+				int i = diagonal;
+				int j = columna + diagonal;
+				int min_ij = Integer.MAX_VALUE;
+				int puntos = 0;
+				for ( int k = i; k <= j; k++ ) {
+					int a = 0, b = 0;
+					puntos += cartas[k];
+					if ( k + 1 <= j ) a = cache[k + 1][j];
+					if ( k - 1 >= i ) b = cache[i][k - 1];
+					min_ij = min( min_ij, a, b );
+				}
+				cache[i][j] = puntos - min_ij;
 			}
-			cache[i][j] = sum(i,j) - min_ij;
-			validCache[i][j] = true;
-			return cache[i][j];
 		}
-		if( i == j && i >= 0 && i < cartas.length ){
-			return cartas[i];
-		}
-		return 0;
 	}
 
-	public void calcularOptimoPuntaje(){
-		puntos = f(0, cartas.length - 1);
+	public void calcularOptimoPuntaje() {
+		construir();
+		puntos = cache[0][cartas.length - 1];
 	}
 
 	public int getOptimoPuntaje(){
